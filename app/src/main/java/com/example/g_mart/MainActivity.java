35491bc35c801e7139b089a2543bf8c6b442e1fe
staru <s.tarunjoseph@gmail.com>
2022-02-28 18:31:39 +0530

    G-mart1

--- app/src/main/java/com/example/g_mart/MainActivity.java
+++ app/src/main/java/com/example/g_mart/MainActivity.java
@@ -6,8 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     private Button signupButton , loginButton;
 
--- app/src/main/java/com/example/g_mart/mainboard.java
+++ app/src/main/java/com/example/g_mart/mainboard.java
@@ -1,24 +1,138 @@
 package com.example.g_mart;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.List;
 
 public class mainboard extends AppCompatActivity {
+
+    FirebaseAuth fAuth;
+    Button btnsignout;
+    private FirebaseFirestore firebaseFirestore;
+    private RecyclerView mFirestoreList;
+    private FirestoreRecyclerAdapter adapter;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainboard);
+
+
+        firebaseFirestore= FirebaseFirestore.getInstance();
+        mFirestoreList = findViewById(R.id.firestorel);
+        btnsignout=findViewById(R.id.btnsignout);
+        fAuth=FirebaseAuth.getInstance();
+
+        btnsignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fAuth.signOut();
+                signOutUser();
+
+
+            }
+        });
+
+        CollectionReference query = firebaseFirestore.collection("Products");
+
+        FirestoreRecyclerOptions<ProductsModel> options= new FirestoreRecyclerOptions.Builder<ProductsModel>()
+                .setQuery(query,ProductsModel.class)
+                .build();
+
+        adapter = new FirestoreRecyclerAdapter<ProductsModel, ProductsViewHolder>(options) {
+            @NonNull
+            @Override
+            public ProductsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_single,parent,false);
+                return new ProductsViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull ProductsViewHolder holder, int position, @NonNull ProductsModel model) {
+                Glide.with(holder.img.getContext()).load(model.getImg()).into(holder.img);
+                holder.list_name.setText(model.getName());
+                holder.list_price.setText(model.getPrice()+"");
+
+            }
+        };
+        mFirestoreList.setHasFixedSize(true);
+        mFirestoreList.setLayoutManager(new LinearLayoutManager(this));
+        mFirestoreList.setAdapter(adapter);
+
+
+
     }
 
-    public void logout(View view){
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(),login.class));
+
+
+    private void signOutUser() {
+        Intent mainActivity =new Intent(mainboard.this, MainActivity.class);
+        mainActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainActivity);
         finish();
     }
+
+
+    private class ProductsViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView img;
+        private TextView list_name;
+        private TextView list_price;
+        public ProductsViewHolder(@NonNull View itemView){
+            super(itemView);
+            img=(ImageView)itemView.findViewById(R.id.image);
+            list_name=itemView.findViewById(R.id.list_name);
+            list_price=itemView.findViewById(R.id.list_price);
+        }
+
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        adapter.stopListening();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
 }
